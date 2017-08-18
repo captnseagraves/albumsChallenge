f781856be62a076092b80aa1002b60f234469a81
Kevin Seagraves <k.s.seagraves@gmail.com>
2017-08-18 11:25:09 -0600

    GET to id working

--- routes/albums.js
+++ routes/albums.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+const knex = require('../knex.js');
 
 /* GET home page. */
 
@@ -7,10 +8,21 @@ var router = express.Router();
 router.get('/:id', (req, res, next) => {
   let id = req.params.id
   knex('albums')
-    .where('id', id)
+    .where('albums.id', id)
+    .join('artists', 'albums.artist_id', 'artists.id' )
+    .join('genres', 'albums.genre_id', 'genres.id' )
+    .select('albums.albumName', 'artists.artistName', 'genres.genreName', 'albums.year')
     .then((selectedAlbum) => {
       res.send(selectedAlbum)
     })
 })
+
+
+const getUserCats = (userId) => {
+    return knex('user_category')
+        .join('categories', 'categories.id', 'user_category.category_id')
+        .where('user_category.user_id', userId)
+}
+
 
 module.exports = router;
